ee2408e0babc210a9a70c26df5f36a5e3f8e3e6c
Werver96 <87260434+Werver96@users.noreply.github.com>
2022-04-14 09:35:44 +0300

    pc menu 320-1200px

--- js/script.js
+++ js/script.js
@@ -28,26 +28,40 @@ const isMobile = {
 /*Добавляем класс для body в зависимости от того с какого устройства 
 открыта страница*/
 if (isMobile.any()){
+/*-Добавляем класс _touch если открыли с устройств с touch screen---*/
 	document.body.classList.add('_touch');
+/*-Если устройсво с touch screen добавляем к классу .menu_arrow класс
+_active-------------------------------------------------------------*/
+/*----------------------Находми нашу стрелочку----------------------*/
 	let menuArrows = document.querySelectorAll('.menu_arrow');
+/*----------Проверяем есть ли у нас хотя бы одна стрелочка----------*/
 	if(menuArrows.length>0){
+/*----------------------Если есть включаем цикл---------------------*/
 		for (let index = 0; index < menuArrows.length; index++) {
 			const menuArrow = menuArrows[index];
+/*----При нажатии на стрелочку добавляется класс _active для родителя 
+элемента, в данном случае пункта меню-------------------------------*/
 			menuArrow.addEventListener("click", function(e){
 				menuArrow.parentElement.classList.toggle('_active');
-
 			});
 		}
 	}
+/*---------Добавляем класс _pc если открыли с устройств с pc--------*/
 } else{
 	document.body.classList.add('_pc');
 }
 /*Меню бургер*/
+/*--------------------Находим нашу кнопку буркер--------------------*/
 const iconMenu = document.querySelector('.menu_icon');
 if (iconMenu){
+/*-----------------------Ищем наш блок с меню-----------------------*/
 	const menuBody = document.querySelector('.header_nav_main');
+/*------При клике на нашему иконке бургера выполняется функция------*/
 	iconMenu.addEventListener("click", function(e) {
+/*------Запрещаем прокрутку страницы когда нажата бургер кнопка-----*/
 		document.body.classList.toggle('_lock');
+/*-------Добавляем класс _active для блока с меню и бургер кнопки при 
+----------------------нажатии на бургер кнопку----------------------*/
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
 	});
