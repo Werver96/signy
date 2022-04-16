af8f7fdccb5d752de3f437db561b63bd966c6805
Werver96 <87260434+Werver96@users.noreply.github.com>
2022-04-16 20:47:14 +0300

    close menu all

--- js/fix.js
+++ js/fix.js
@@ -1,17 +1,21 @@
-/*-----------Скрываем меню при переходе по навигации-----*/
+/*-----------Скрываем меню при переходе по навигации меню а так же вход и регистрации-----*/
 //Сделано для того чтобы на главной странице при переходе по ссылке цены/регистрация/вход меню закрывалось
 linksMenu=document.getElementsByClassName('header_nav_link')
 for (var i = 0; i < linksMenu.length; i++) {
     linkMenu=linksMenu[i];
     linkMenu.onclick=closeMenu;
 }
+
+linksRegIn=document.querySelectorAll('.popup-link');
+for (var i = 0; i < linksRegIn.length; i++) {
+    linkRegIn=linksRegIn[i];
+    linkRegIn.onclick=closeMenu;
+}
+/*-------------------------------------------------------*/
+/*Скрываем меню при переходе по регистрации/вход---------*/
 function closeMenu() {
      document.getElementById('menu').classList.remove('_active');
      document.body.classList.remove('_lock');
      document.querySelector('.menu_icon').classList.remove('_active')
 
 }
-
-/*-------------------------------------------------------*/
-/*Скрываем меню при переходе по регистрации/вход---------*/
-//linksRegIn=document.querySelector('header_nav_link')
