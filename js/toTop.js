b1622f67041dee5527af5239e3167e1a275c12f5
Werver96 <87260434+Werver96@users.noreply.github.com>
2022-04-17 11:09:21 +0300

    button toTop js

---
+++ js/toTop.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    //window.scrollTo(x,y)
+    var scrolled;
+    var timer;
+
+    document.getElementById('top').onclick = function(){
+        scrolled = window.pageYOffset;
+        //window.scrollTo(0,0);
+        scrollToTop();
+    }
+    function scrollToTop(){
+        if (scrolled > 0) {
+            window.scrollTo(0, scrolled);
+            scrolled = scrolled - 400; //100 - скорость прокрутки
+            timer = setTimeout(scrollToTop, 20);
+        }
+        else {
+            clearTimeout(timer);
+            window.scrollTo(0,0);
+        }
+    }
+}
